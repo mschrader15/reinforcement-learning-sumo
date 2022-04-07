66946a820d91ca2bf5f186d964fbe9ce41dc5fe6
Max Schrader <mcschrader@crimson.ua.edu>
2022-04-07 17:46:54 -0500

    Fixed fresh install

--- rl_sumo/core/__init__.py
+++ rl_sumo/core/__init__.py
@@ -1,6 +1,4 @@
 from .kernel import Kernel
 from .observers.observer import GlobalObservations
 from .observers.MaxPressureObserver import MaxPressureGlobalObservations
-from .actor import GlobalActor
-from .DualRingActor import GlobalDualRingActor
 from .rewarder import *
