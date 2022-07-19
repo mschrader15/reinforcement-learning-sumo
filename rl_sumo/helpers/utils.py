76ed5b3050a1dbf71fcbafb51c5f732960b796dd
Max <mcschrader@crimson.ua.edu>
2022-07-19 08:32:33 -0500

    fixes NEMA phase control mapping
    Signed-off-by: Max <mcschrader@crimson.ua.edu>

--- rl_sumo/helpers/utils.py
+++ rl_sumo/helpers/utils.py
@@ -5,6 +5,7 @@ This file contains various utilities for dealing with rllib files
 """
 import os
 from typing import OrderedDict
+from unittest import skip
 import json5 as json
 
 
@@ -71,11 +72,20 @@ def read_nema_config(path: str, tlsID: str = "") -> OrderedDict:
         p["@key"] : p["@value"] for p in tl_dict['param']
     }
 
+    # find "skip" phases. These are phases that are always green.
+    skip_phases = []
+    for i, p in enumerate(tl_dict['phase'][:-1]):
+        skip_phases.extend(iid for iid, (_p, _pnext) in enumerate(zip(p["@state"], tl_dict['phase'][i + 1]["@state"])) if _p == "G" and _pnext == "G")
+    skip_phases = set(skip_phases)
+
+    # skip_phases = [iid for i, p in enumerate(tl_dict['phase'][:-1]) for iid, (_p, _pnext) in enumerate(zip(p["@state"], tl_dict['phase'][i+1]["@state"])) if _p == _pnext]
+
+
     # add a parameter called the controlling index, useful for mapping phase names to lanes
     # this might not always be right, but can't think of immediately better way to do it
     for phase in tl_dict['phase']:
-        phase['controlling_index'] = [i for i, s in enumerate(phase["@state"]) if s == "G"]
-    
+        phase['controlling_index'] = [i for i, s in enumerate(phase["@state"]) if s == "G" and i not in skip_phases]
+
     # CRITICAL, sort the phases by their controlling indexs
     tl_dict['phase'] = OrderedDict(
         p[:-1] for p in sorted(((phase["@name"], phase, min(phase['controlling_index'])) for phase in tl_dict['phase']), key=lambda x: x[-1])
